0401abc2f0db33ade7e9311e0f976c2c93c094fb
devidardhruvil-codes <dsilento01@gmail.com>
2025-06-21 11:52:11 +0530

    3D Model, HERO SECTION, SHOWCASE SECTION, NAVBAR added

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Navbar from "./components/Navbar";
+import Hero from "./sections/Hero";
+import ShowcaseSection from "./sections/ShowcaseSection";
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <ShowcaseSection />
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,48 @@
+import { navLinks } from "../constants";
+import { useEffect, useState } from "react";
+
+const Navbar = () => {
+  const [scrolled, setscrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const isScrolled = window.scrollY > 10;
+      setscrolled(true);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  });
+
+  return (
+    <header className={`navbar ${scrolled ? "scrolled" : "not-scrolled"}`}>
+      <div className="inner">
+        <a className="logo" href="#hero">
+          Dhruvil | Devidar
+        </a>
+
+        <nav className="desktop">
+          <ul>
+            {navLinks.map(({ link, name }) => (
+              <li key={name} className="group">
+                <a href={link}>
+                  <span>{name}</span>
+                  <span className="underline" />
+                </a>
+              </li>
+            ))}
+          </ul>
+        </nav>
+
+        <a href="#contact" className="contact-btn group">
+          <div className="inner">
+            <span>Contact me</span>
+          </div>
+        </a>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
